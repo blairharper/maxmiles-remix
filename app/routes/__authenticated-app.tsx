3ed521d81bd37870a5675116a0e9a27046d62743
Blair Harper <blair@blairharper.dev>
2023-02-11 14:29:42 +0000

    make nav paths absolute, and make sure user is authenticated

--- app/routes/__authenticated-app.tsx
+++ app/routes/__authenticated-app.tsx
@@ -3,6 +3,14 @@ import { Disclosure, Menu, Transition } from "@headlessui/react";
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
 import { Outlet } from "@remix-run/react";
 import { useLocation } from "@remix-run/react";
+import { requireUserId } from "~/session.server";
+import type { LoaderArgs } from "@remix-run/server-runtime";
+import { json } from "@remix-run/node";
+
+export const loader = async ({ request }: LoaderArgs) => {
+  await requireUserId(request);
+  return json({});
+};
 
 const user = {
   name: "Tom Cook",
@@ -11,10 +19,10 @@ const user = {
     "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
 };
 const navigation = [
-  { name: "Dashboard", href: "dashboard", current: false },
-  { name: "My Cards", href: "my-cards", current: false },
-  { name: "Deals", href: "deals", current: false },
-  { name: "Account", href: "account", current: false },
+  { name: "Dashboard", href: "/dashboard", current: false },
+  { name: "My Cards", href: "/my-cards", current: false },
+  { name: "Deals", href: "/deals", current: false },
+  { name: "Account", href: "/account", current: false },
 ];
 const userNavigation = [
   { name: "Your Profile", href: "#" },
